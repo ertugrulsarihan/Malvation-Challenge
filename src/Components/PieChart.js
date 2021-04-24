a6e484979c3b357d4af7d53d5c6876620026e42e
Ertuğrul Sarıhan <ertugrulsarihan1@gmail.com>
2021-04-24 03:07:52 +0300

    m

--- src/Components/PieChart.js
+++ src/Components/PieChart.js
@@ -4,26 +4,26 @@ import { Doughnut } from "react-chartjs-2";
 export default function PieChart({ cost, listNames, listPrices }) {
   const [chartdata, setChartData] = useState([]);
 
-  const chart = () => {
-    setChartData({
-      labels: listNames,
-      datasets: [
-        {
-          label: "Expenses",
-          data: listPrices,
-          backgroundColor: [
-            "rgba(75,192,192,1)",
-            "rgb(255, 99, 132)",
-            "rgb(54, 162, 235)",
-            "rgb(255, 205, 86)",
-          ],
-          borderWidth: 4,
-          hoverOffset: 4,
-        },
-      ],
-    });
-  };
   useEffect(() => {
+    const chart = () => {
+      setChartData({
+        labels: listNames,
+        datasets: [
+          {
+            label: "Expenses",
+            data: listPrices,
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+            borderWidth: 4,
+            hoverOffset: 4,
+          },
+        ],
+      });
+    };
     chart();
   }, [cost, listNames, listPrices]);
 
